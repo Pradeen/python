f2a5a7cea95a9e06c698af1ffd964f70ba7cfdbd
pradeen ramasamy <r.pradeen@hotmail.com>
2019-01-09 01:43:17 +0000

    file name changes

--- app.py
+++ app.py
@@ -1,24 +1,17 @@
 from flask import Flask
-from redis import Redis, RedisError
-import os
-import socket
-
-# Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
-
+from flask import request, jsonify
+from flask import render_template
 app = Flask(__name__)
-
 @app.route("/")
 def hello():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
-
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+    return render_template("home.html")
+@app.route("/createOrders",  methods=['GET', 'POST','DELETE', 'PATCH'])
+def createOrder():
+    return "ordpyters are created sucessfully"
+@app.route("/home",methods=['GET'])
+def home():
+    return render_template("home.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.debug=True
+    app.run(host='0.0.0.0',port=9090)
